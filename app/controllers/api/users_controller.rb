761c6322d1e9c6bf6d7e6161c7f834a8ede30bc7
Samuel Fortunato <samfortunato@gmail.com>
2019-01-24 15:42:26 -0500

    Fix comma in users_controller.rb

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,9 @@
 class Api::UsersController < ApplicationController
   def create
     @user = User.new(
-      first_name: params[:user][:first_name]
-      last_name: params[:user][:last_name]
-      email: params[:user][:email]
+      first_name: params[:user][:first_name],
+      last_name: params[:user][:last_name],
+      email: params[:user][:email],
       password: params[:user][:password]
     )
 
