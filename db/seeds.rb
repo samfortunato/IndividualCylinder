6287f20f1d953794b58cb93ea2042e965c3c445e
Samuel Fortunato <samfortunato@gmail.com>
2019-01-25 10:31:37 -0500

    Add demo user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+demo_user = User.create!(
+  first_name: 'Demo',
+  last_name: 'User',
+  email: 'demo-user@mail.com',
+  password: '12345678'
+)
