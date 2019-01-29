060edce8e89818380706d41daa41bfb29521bb0b
Samuel Fortunato <samfortunato@gmail.com>
2019-01-29 10:54:29 -0500

    Fix keyboard shortcut event listener persisting on component unmounting

--- frontend/components/videos/video_show/video_player_OLD_COPY.jsx
+++ frontend/components/videos/video_show/video_player_OLD_COPY.jsx
@@ -7,7 +7,6 @@ class VideoPlayer extends React.Component {
     super(props);
 
     this.initiateVideoInterface = this.initiateVideoInterface.bind(this);
-    this.handleKeyboardShortcuts = this.handleKeyboardShortcuts.bind(this);
   }
 
   handlePlayPause() {
@@ -20,7 +19,7 @@ class VideoPlayer extends React.Component {
     video.muted = !video.muted;
 
     const muteIcon = document.querySelector('#mute i');
-
+    
     if (video.muted) {
       muteIcon.classList.replace('fa-volume-up', 'fa-volume-mute');
     } else {
@@ -48,10 +47,10 @@ class VideoPlayer extends React.Component {
   setFullScreenData(videoContainer, state) {
     videoContainer.setAttribute('data-fullscreen', !!state);
   }
-
+  
   handleFullScreen() {
     const videoContainer = document.querySelector('#video-container');
-
+    
     if (this.isFullScreen()) {
       document.exitFullscreen();
       this.setFullScreenData(videoContainer, false);
@@ -60,48 +59,7 @@ class VideoPlayer extends React.Component {
       this.setFullScreenData(videoContainer, true);
     }
   }
-
-  handleKeyboardShortcuts(e) {
-    switch (e.key) {
-      case 'k':
-        this.handlePlayPause();
-        break;
-
-      case '':
-        this.handlePlayPause();
-        break;
-
-      case 'm':
-        this.handleMute();
-        break;
-
-      case 'j':
-        if (video.currentTime >= 10) {
-          video.currentTime -= 10;
-        } else {
-          video.currentTime = 0;
-        }
-
-        break;
-
-      case 'l':
-        if (video.currentTime < video.duration) {
-          video.currentTime += 10;
-        } else {
-          video.currentTIme = video.duration;
-        }
-
-        break;
-
-      case 'f':
-        this.handleFullScreen();
-        break;
-
-      default:
-        break;
-    }
-  }
-
+  
   initiateVideoInterface() {
     const video = document.querySelector('#video');
 
@@ -113,7 +71,7 @@ class VideoPlayer extends React.Component {
     const currentVideoTime = document.querySelector('.current-video-time');
     const videoDuration = document.querySelector('.video-duration');
     const fullScreen = document.querySelector('#full-screen');
-
+    
     // Disable right click on video
     video.addEventListener('contextmenu', (e) => {
       e.preventDefault();
@@ -145,7 +103,7 @@ class VideoPlayer extends React.Component {
     video.addEventListener('click', () => {
       this.handlePlayPause();
     });
-
+    
     playPause.addEventListener('click', () => {
       this.handlePlayPause();
     });
@@ -173,7 +131,7 @@ class VideoPlayer extends React.Component {
     video.addEventListener('timeupdate', () => {
       currentVideoTime.textContent = toMMSSTimeString(video.currentTime);
     });
-
+    
     // -- --
 
     fullScreen.addEventListener('click', this.handleFullScreen.bind(this));
@@ -184,7 +142,48 @@ class VideoPlayer extends React.Component {
 
     // Keyboard shortcuts
 
-    document.addEventListener('keydown', this.handleKeyboardShortcuts);
+    document.addEventListener('keydown', (e) => {
+      // console.log(e.key);
+      
+      switch (e.key) {
+        case 'k':
+          this.handlePlayPause();
+          break;
+          
+        case '':
+          this.handlePlayPause();
+          break;
+          
+        case 'm':
+          this.handleMute();
+          break;
+          
+        case 'j':
+          if (video.currentTime >= 10) {
+            video.currentTime -= 10;
+          } else {
+            video.currentTime = 0;
+          }
+
+          break;
+          
+        case 'l':
+          if (video.currentTime < video.duration) {
+            video.currentTime += 10;
+          } else {
+            video.currentTIme = video.duration;
+          }
+          
+          break;
+          
+        case 'f':
+          this.handleFullScreen();
+          break;
+          
+        default:
+          break;
+      }
+    });
   }
 
   enablePlayPauseIconUpdate() {
@@ -208,54 +207,42 @@ class VideoPlayer extends React.Component {
     this.initiateVideoInterface();
     this.enablePlayPauseIconUpdate();
   }
-
-  componentWillUnmount() {
-    // Cloning & replacing to remove all event listeners
-    //   from video player container and it's children
-    const videoContainer = document.querySelector('#video-container');
-    const videoContainerClone = videoContainer.cloneNode(true);
-
-    videoContainer.parentNode
-      .replaceChild(videoContainerClone, videoContainer);
-
-    document.removeEventListener('keydown', this.handleKeyboardShortcuts);
-  }
-
+  
   render() {
     const { videoUrl } = this.props;
-
+    
     const videoPlayer = (
-      <figure id="video-container">
-        <video
-          id="video"
-          src={videoUrl}
-          preload="metadata">
-
-          {/* <source
+        <figure id="video-container">
+          <video
+            id="video"
+            src={videoUrl}
+            preload="metadata">
+            
+            {/* <source
               src={videoUrl}
               type="video/mp4"
             /> */}
 
-          Your browser does not support video playback. Upgrade to a browser that supports video playback to view this video.
+            Your browser does not support video playback. Upgrade to a browser that supports video playback to view this video.
           </video>
 
-        <div id="video-interface">
-          <progress id="video-progress" value="0" min="0"></progress>
+          <div id="video-interface">
+            <progress id="video-progress" value="0" min="0"></progress>
 
-          <ul className="main-video-interface">
-            <li><button id="play-pause" type="button"><i className="fas fa-play"></i></button></li>
-            <li><button id="mute" type="button"><i className="fas fa-volume-up"></i></button></li>
-            <li><input id="volume-slider" type="range" min="0" max="1" step="0.01" /></li>
-            <li>
-              <span className="current-video-time">00:00</span>
-              <span className="video-duration">00:00</span>
-            </li>
-            <li><button id="full-screen" type="button"><i className="far fa-square"></i></button></li>
-          </ul>
-        </div>
-      </figure>
+            <ul className="main-video-interface">
+              <li><button id="play-pause" type="button"><i className="fas fa-play"></i></button></li>
+              <li><button id="mute" type="button"><i className="fas fa-volume-up"></i></button></li>
+              <li><input id="volume-slider" type="range" min="0" max="1" step="0.01" /></li>
+              <li>
+                <span className="current-video-time">00:00</span>
+                <span className="video-duration">00:00</span>
+              </li>
+              <li><button id="full-screen" type="button"><i className="far fa-square"></i></button></li>
+            </ul>
+          </div>
+        </figure>
     );
-
+    
     return (
       <>
         {videoPlayer}
