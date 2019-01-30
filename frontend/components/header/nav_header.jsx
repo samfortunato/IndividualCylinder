e54dcc9c00faa263ea7f75019abfb16eedb36498
Samuel Fortunato <samfortunato@gmail.com>
2019-01-30 15:52:57 -0500

    Update header nav icons

--- frontend/components/header/nav_header.jsx
+++ frontend/components/header/nav_header.jsx
@@ -9,7 +9,7 @@ class NavHeader extends React.Component {
       <header className="main-header">
         <nav className="global-nav">
           <ul>
-            <li><img className="menu-icon" src="https://placeimg.com/24/24/tech" alt="Menu icon" /></li>
+            <li><i class="fas fa-bars"></i></li>
             <li>
               <Link className="main-header-logo-home-link" to="/">
                 <img className="main-header-logo" src="https://i.imgur.com/k5ZfpMM.png" alt="InvidivualCylinder logo" />
@@ -23,7 +23,7 @@ class NavHeader extends React.Component {
         <nav className="user-actions">
           <ul>
             <li>
-              <Link to="/upload"><i className="fas fa-upload"></i></Link>
+              <Link to="/upload"><i className="fas fa-video"></i></Link>
             </li>
             <li><UserActionsMenuContainer /></li>
           </ul>
