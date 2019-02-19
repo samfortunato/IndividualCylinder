f74e3cc567cff58774f40423aa2c56e5d5c84214
Samuel Fortunato <samfortunato@gmail.com>
2019-02-19 17:07:16 -0500

    Update frontend state when user subscribes/unsubscribes

--- frontend/reducers/channels_reducer.js
+++ frontend/reducers/channels_reducer.js
@@ -4,12 +4,37 @@ import {
   RECEIVE_CHANNEL
 } from '../actions/channels_actions';
 
+import {
+  RECEIVE_SUBSCRIPTION,
+  REMOVE_SUBSCRIPTION
+} from '../actions/subscriptions_actions';
+
 const channelsReducer = (currentState = {}, action) => {
   Object.freeze(currentState);
 
   switch (action.type) {
     case RECEIVE_CHANNEL: {
       return merge({}, currentState, action.channel);
+    }
+
+    case RECEIVE_SUBSCRIPTION: {
+      let nextState = merge({}, currentState);
+      const { channel_id: channelId } = action.subscription;
+      
+      nextState[channelId].current_user_is_subscribed = true;
+      nextState[channelId].subscriber_amount++;
+      
+      return nextState;
+    }
+
+    case REMOVE_SUBSCRIPTION: {
+      let nextState = merge({}, currentState);
+      const { channel_id: channelId } = action.subscription;
+
+      nextState[channelId].current_user_is_subscribed = false;
+      nextState[channelId].subscriber_amount--;
+
+      return nextState;
     }
     
     default:
