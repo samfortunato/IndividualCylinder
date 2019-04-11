1db325385fd8d32d19d46dabaf7b05021a6138de
Samuel Fortunato <samfortunato@gmail.com>
2019-04-11 15:45:23 -0400

    Hide sidebar nav menu icon until feature is completed

---
+++ frontend/components/side_navigation/side_navigation.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SideNavigation extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>side nav</div>
+    );
+  }
+}
+
+export default SideNavigation;
