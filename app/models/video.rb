604429a3d3dc649100aafa595cee031f4401a12e
Samuel Fortunato <samfortunato@gmail.com>
2025-06-17 00:41:40 -0700

    Ensure video file validation error messages are put into errors collection; visually block validations and associations together

--- app/models/video.rb
+++ app/models/video.rb
@@ -14,29 +14,24 @@
 class Video < ApplicationRecord
   validates :title, :description,
     presence: true
-    
   validate :ensure_video_file
 
   belongs_to :user,
     class_name: 'User',
     foreign_key: :uploader_id
-
   has_many :comments,
     dependent: :destroy
-  
   has_many :likes,
     as: :likable,
     dependent: :destroy
-
   has_one_attached :video_file
-
   has_one_attached :video_thumbnail
 
   private
 
   def ensure_video_file
     if !self.video_file.attached?
-      errors[:video_file] << 'must be attached'
+      errors.add(:video_file, "must be attached")
     end
   end
 end
