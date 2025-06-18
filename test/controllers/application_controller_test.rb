41bff1d69f13b0199fe9cbb986e55f2dbdfefbba
Samuel Fortunato <samfortunato@gmail.com>
2025-06-18 01:28:35 -0700

    Add REALLY STUPID `ApplicationController` test todo because testing `ApplicationController` is apparently REALLY STUPID

---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class ApplicationControllerTest < ActionDispatch::IntegrationTest
+  test "can get the current user" do
+    # user = users(:one)
+    # session[:session_token] = user.session_token
+
+    # get :index
+    # current_user = @controller.current_user
+
+    # assert_equal current_user, user
+
+    skip "TODO: i can't believe testing `ApplicationController` directly is this complicated.\n" \
+      "how RETARDED. figure out how to do this later? IDK!"
+  end
+end
